36d6351b64adb441a969a1e9b72e81a34a6e4d08
YvarRavy <56068734+YvarRavy@users.noreply.github.com>
2021-01-24 12:16:54 +0100

    Update led.ino

--- led/led.ino
+++ led/led.ino
@@ -4,25 +4,11 @@ const int buttonPin = 2;
 int ledState = LOW;        // sets led
 int buttonState = 0;
 
-unsigned long previousMillis = 0;
-
-const long interval = 10;
-
 void setup() {
   pinMode(ledPin, OUTPUT);
 //  pinMode(buttonPin, INPUT);
 }
 void loop() {
-  unsigned long currentMillis = millis();
-  
-
-  if buttonState == 1 then lkeydown=true
-  if lkeydown and buttonstate == 0 then lbuttonpressed
-  if buttonpressed ledState = !ledState
-  
-  if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-
       buttonState = digitalRead(buttonPin);
       
       if (buttonState == HIGH) {
