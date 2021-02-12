addcdf305884f277c44cfb7e5c5c9441567ad7f5
YvarRavy <56068734+YvarRavy@users.noreply.github.com>
2021-02-12 14:12:30 +0100

    Update sosLED.ino

--- sosLED/sosLED.ino
+++ sosLED/sosLED.ino
@@ -8,38 +8,38 @@ void loop()
 {
     digitalWrite(Led, HIGH);  //dot
     delay (500);              //dotlengte
-    digitalWrite(Led, LOW);   //symboolspatie
-    delay(500);               //symboolspatie lengte = dotlengte
+    digitalWrite(Led, LOW);   
+    delay(500);               
     digitalWrite(Led, HIGH);  //dot
     delay (500);
-    digitalWrite(Led, LOW);
+    digitalWrite(Led, LOW);   //dot
     delay(500);
     digitalWrite(Led, HIGH);  //dot
     delay (500);
-    digitalWrite(Led, LOW);   //letterspatie
-    delay(1500);              //letterspatie lengte = 3 x dotlengte
-    digitalWrite(Led, HIGH);  //dash
-    delay (1500);             //dashlengte is 3x dotlengte
-    digitalWrite(Led, LOW);   //symboolspatie
-    delay(500);               //symboolspatie lengte = dotlengte
-    digitalWrite(Led, HIGH);  //dash
+    digitalWrite(Led, LOW);  //dash
+    delay(1500);              //dash lengte
+    digitalWrite(Led, HIGH); 
+    delay (1500);             
+    digitalWrite(Led, LOW);   
+    delay(500);               
+    digitalWrite(Led, HIGH);
     delay (1500);
     digitalWrite(Led, LOW);
     delay(500);
-    digitalWrite(Led, HIGH);  //dash
+    digitalWrite(Led, HIGH);
     delay (1500);
-    digitalWrite(Led, LOW);   //letterspatie
+    digitalWrite(Led, LOW); 
     delay(1500);
-    digitalWrite(Led, HIGH);  //dot
+    digitalWrite(Led, HIGH);
     delay (500);
     digitalWrite(Led, LOW);
     delay(500);
-    digitalWrite(Led, HIGH);  //dot
+    digitalWrite(Led, HIGH); 
     delay (500);
     digitalWrite(Led, LOW);
     delay(500);
-    digitalWrite(Led, HIGH);  //dot
+    digitalWrite(Led, HIGH); 
     delay (500);
     digitalWrite(Led, LOW);   //woordspatie
-    delay(3500);              //woordspatie lengt = 7x dotlengte
+    delay(3500);              //woordspatie lengte
 }
