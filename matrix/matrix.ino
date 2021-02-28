356c76f8bcff3e9239a9669fbbdea4578a92a6a7
YvarRavy <56068734+YvarRavy@users.noreply.github.com>
2021-02-28 17:46:57 +0100

    Update matrix.ino

--- matrix/matrix.ino
+++ matrix/matrix.ino
@@ -3,40 +3,19 @@
 LedControl lc = LedControl (12, 11, 10, 1);
 int postion = 0;
 int row;
+
 void setup() {
-
   Serial.begin (9600);
-
   lc.shutdown (0, false);
-
   // set the brightness to a low value
   lc.setIntensity (0, 0); // 0 = low; 8 = high
-
-  // clear the display // goodbye smiley!
+  // clear the display
   lc.clearDisplay(0);
 }
-
-// here is the series of instructions for the continuously running part of the sketch
 
 void loop() {
-
-  for (postion = 0; postion <= 8; postion++) {
-    Serial.println(postion);
-    //lc.setLed(0, 1, 1, false); // erase previous ball position
-    delay(500);
-
-    lc.setLed(0, row, postion, true);
-    if (postion == 8) {
-      test();
-    }
-  }
-
-
-}
-
-void test() {
-  delay(500);
-  lc.clearDisplay(0);
-  row++;
-
+  lc.setLed(0, 3, 4, true);
+  lc.setLed(0, 3, 3, true);
+  lc.setLed(0, 4, 4, true);
+  lc.setLed(0, 4, 3, true);
 }
